7470d11f3ac99f093055346b3da479d5d4459f3e
Florian Lutz <florian.lutz@ymail.com>
2024-06-04 14:32:23 +0200

    create basic app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Animal } from './animal';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'zoobook-angular';
+  title = 'Zoobuch';
+  animals: Animal[] = [
+    {
+      name: 'Hansi',
+      type: 'ape',
+    },
+    {
+      name: 'Schildi',
+      type: 'turtle',
+    },
+    {
+      name: 'Joane',
+      type: 'elefant',
+    },
+  ];
+  newAnimal: Animal = {
+    name: '',
+    type: '',
+  };
+
+  deleteAnimal(animal: Animal): void {
+    this.animals = this.animals.filter((a) => a != animal);
+  }
+
+  addAnimal() {
+    this.animals.push(this.newAnimal);
+  }
 }
